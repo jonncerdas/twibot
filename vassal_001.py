a3d209988a61e409615ba809a11d51a81d970266
jonncerdas <jonnathan@cerdas.cr>
2020-11-02 20:24:09 -0600

    fix linters & add requirements

--- vassal_001.py
+++ vassal_001.py
@@ -1,29 +1,29 @@
-import tweepy
-import requests
-from newspaper import Article
 import os
 
-auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
-auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
+import requests
+import tweepy
+from newspaper import Article
+
+auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'],
+                           os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ['ACCESS_TOKEN'],
+                      os.environ['ACCESS_TOKEN_SECRET'])
 
 api = tweepy.API(auth)
 
-url = 'https://www.nationalgeographic.com/photography/photo-of-the-day/'
-article = Article(url)
+URL = 'https://www.nationalgeographic.com/photography/photo-of-the-day/'
+article = Article(URL)
 article.download()
 article.parse()
 
-filename = 'temp.jpg'
+FILENAME = 'temp.jpg'
 request = requests.get(article.top_image, stream=True)
 
 if request.status_code == 200:
-    with open(filename, 'wb') as image:
+    with open(FILENAME, 'wb') as image:
         for chunk in request:
             image.write(chunk)
 
-
-    media = api.media_upload(filename)
-    tweet = "Great scifi author or greatest scifi author? #williamgibson"
-    post_result = api.update_status(status=tweet, media_ids=[media.media_id])
-
-
+    media = api.media_upload(FILENAME)
+    TWEET = "Great scifi author or greatest scifi author? #williamgibson"
+    post_result = api.update_status(status=TWEET, media_ids=[media.media_id])
--- vassal_002.py
+++ vassal_002.py
@@ -1,36 +1,29 @@
+import os
+import random
+
 import requests
 import tweepy
-from newspaper import Article
-import random
-import os
 
 qod_categories = [
-    'inspire',
-    'management',
-    'sports',
-    'life',
-    'funny',
-    'love',
-    'art',
+    'inspire', 'management', 'sports', 'life', 'funny', 'love', 'art',
     'students'
 ]
 
 category = random.choice(qod_categories)
 
-params = {
-    'category': category,
-    'language':'en'
-}
+params = {'category': category, 'language': 'en'}
 
 resp = requests.get(url='https://quotes.rest/qod', params=params).json()
 
-qod = '"{}" {} \n #{} #quote'.format(resp['contents']['quotes'][0]['quote'], resp['contents']['quotes'][0]['author'], category)
+QOD = '"{}" {} \n #{} #quote'.format(resp['contents']['quotes'][0]['quote'],
+                                     resp['contents']['quotes'][0]['author'],
+                                     category)
 
-auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
-auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
+auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'],
+                           os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ['ACCESS_TOKEN'],
+                      os.environ['ACCESS_TOKEN_SECRET'])
 
 api = tweepy.API(auth)
 
-api.update_status(status=qod)
-
-
+api.update_status(status=QOD)
